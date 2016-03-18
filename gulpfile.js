394cc04083101bc08c7ece3e98c8290cfd0fc4af
Cyril Balit <cbalit@gmail.com>
2016-03-18 21:24:30 +0100

    start moving stuffs in app folder

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,7 @@ var connect = require('gulp-connect');
 // concat et copie toutes les resources nécessaires au déploiement dans ./build
 gulp.task("build", function build() {
   return all([
-    gulp.src('partials/*.html')
+    gulp.src('app/**/*.html')
       .pipe(inlineAngularTemplates('index.html', {
            base: 'partials',
            prefix: 'partials/',
@@ -26,40 +26,40 @@ gulp.task("build", function build() {
            }
        }))
       .pipe(htmlreplace({
-        'css': ['css/vendor.css', 'css/app.css'],
-        'js': ['js/vendor.js', 'js/app.js']
+        'css': ['app/css/vendor.css', 'app/css/app.css'],
+        'js': ['app.js/vendor.js', 'app/js/app.js']
       }))
       .pipe(gulp.dest('build/')),
 
-    gulp.src('js/*.js')
+    gulp.src('app/js/*.js')
       .pipe(concat("app.js"))
       .pipe(gulp.dest('build/js/')),
 
-    gulp.src('css/*.css')
+    gulp.src('app/css/*.css')
       .pipe(concat("app.css"))
       .pipe(gulp.dest('build/css/')),
 
-    gulp.src('js/d3/*.js')
+    gulp.src('app/js/d3/*.js')
       .pipe(gulp.dest('build/js/d3/')),
 
-    gulp.src(['./bower_components/angular/angular.min.js',
-          'bower_components/angular-route/angular-route.min.js',
-          'bower_components/angular-animate/angular-animate.min.js',
-          'bower_components/angular-aria/angular-aria.min.js',
-          'bower_components/angular-messages/angular-messages.min.js',
-          'bower_components/angular-material/angular-material.min.js'])
+    gulp.src(['app/bower_components/angular/angular.min.js',
+          'app/bower_components/angular-route/angular-route.min.js',
+          'app/bower_components/angular-animate/angular-animate.min.js',
+          'app/bower_components/angular-aria/angular-aria.min.js',
+          'app/bower_components/angular-messages/angular-messages.min.js',
+          'app/bower_components/angular-material/angular-material.min.js'])
       .pipe(concat("vendor.js"))
       .pipe(gulp.dest('build/js/')),
 
-    gulp.src(['bower_components/bootstrap/dist/css/bootstrap.min.css',
-             'bower_components/angular-material/angular-material.css'])
+    gulp.src(['app/bower_components/bootstrap/dist/css/bootstrap.min.css',
+             'app/bower_components/angular-material/angular-material.css'])
       .pipe(concat("vendor.css"))
       .pipe(gulp.dest('build/css/')),
 
-    gulp.src('bower_components/bootstrap/dist/fonts/*')
+    gulp.src('app/bower_components/bootstrap/dist/fonts/*')
       .pipe(gulp.dest('build/fonts/')),
 
-    gulp.src('img/*')
+    gulp.src('app/img/*')
       .pipe(gulp.dest('build/img/')),
 
     gulp.src('favicon.ico')
@@ -68,14 +68,16 @@ gulp.task("build", function build() {
 });
 
 gulp.task('watch', function () {
-  gulp.watch('*.html', ['build']);
-  gulp.watch('partials/**/*.html', ['build']);
-  gulp.watch('css/**/*.css', ['build']);
-  gulp.watch('js/**/*.js', ['build']);
+  gulp.watch('app/*.html', ['build']);
+  gulp.watch('app/partials/**/*.html', ['build']);
+  gulp.watch('app/css/**/*.css', ['build']);
+  gulp.watch('app/js/**/*.js', ['build']);
 });
 
 gulp.task('connect', function() {
-  connect.server();
+  connect.server({
+      root: 'app'
+  });
 });
 
 gulp.task('default', ['connect', 'watch']);
